7a244449a09abd837af3f0e3db0af72fd6bf6519
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-11 19:27:16 +0300

    add new Card

---
+++ layout/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Card.module.scss';
+
+export const Card = () => {
+  return (
+    <div className={styles.card}>
+      <div className="container">
+      
+      </div>
+    </div>
+  );
+};
