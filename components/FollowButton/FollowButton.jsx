2e8c801ac1882bebae0c289922d80f49d78dda51
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-11 21:03:03 +0300

    Finish create FollowButton

---
+++ components/FollowButton/FollowButton.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import styles from './FollowButton.module.scss';
+
+export const FollowButton = () => {
+  const [condition, setCondition] = useState(true);
+  const OUTLINED = {
+    status: 'outlined',
+    title: 'Unfollow'
+  };
+
+  const CONTAINED = {
+    status: 'contained',
+    title: 'Follow'
+  };
+
+  const checkStatus = check(CONTAINED.status, OUTLINED.status)
+  const checkTitle = check(CONTAINED.title, OUTLINED.title)
+
+    function check(firstValue, secondValue) {
+      return condition ? firstValue : secondValue;
+    }
+
+    function changeCondition() {
+      setCondition(state => !state)
+    }
+
+  return (
+    <Button
+      className={`${styles.btn} ${styles[checkStatus]}`}
+      variant={checkStatus}
+      onClick={changeCondition}
+    >
+      {checkTitle}
+    </Button>
+  );
+};
